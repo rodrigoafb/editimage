d4664fd18a073d252823d9728b8d375c47b08a04
renanaragao <renan.aragao@RS-DS03.alfa.local>
2015-01-22 14:17:59 -0200

    removido a fábrica de editimage.

--- app/script/EditimageObjeto/EditimageObjeto.js
+++ app/script/EditimageObjeto/EditimageObjeto.js
@@ -1,5 +1,48 @@
 'use strict';
 
-var EditimageObjeto = function(){
-	
+var EditimageObjeto = function(observer){
+
+	if(!observer || typeof observer.notificar !== 'function') throw new Error("Informe o observer.");
+
+	var self = this;
+	var _observer = observer;
+	var shape = new createjs.Shape();
+
+	var _selecionado;
+
+	Object.defineProperties(self, {
+
+		'selecionado': {
+			get: function(){ return _selecionado; },
+			set: function(value){
+
+				if(_selecionado === value) return;
+
+				_selecionado = value;
+
+				_observer.notificar();
+
+			}
+		}
+
+	});
+
+	shape.addEventListener('click', function(e){
+
+		self.selecionado = true;
+
+	});
+
+	self.desenhar = function(){
+
+
+
+	};
+
+	self.retornarShape = function(){
+
+		return shape;
+
+	};
+
 };
--- test/ObjetosTestSpec.js
+++ test/ObjetosTestSpec.js
@@ -1,12 +1,117 @@
 'use strict';
 
+var createjs,
+	observer;
+
 describe('Objetos - ', function () {
 	
+
+	beforeEach(function(){
+
+		createjs = {
+
+			Shape: function(){
+
+				var self = this;
+				var evento = {};
+
+				self.graphics = {
+					beginStroke: function(){
+
+					}
+				};
+
+				self.addEventListener = function(pEvento, callback){
+
+					evento[pEvento] = callback;
+
+				}
+
+				self.dispararEvento = function(pEvento){
+
+					evento[pEvento]();
+
+				};
+
+			}
+
+		};
+
+		observer = {
+			notificar: function(){
+
+				observer.notificado = true;
+
+			}
+		};
+
+	});
+
 	it('Deve criar um EditimageObjeto', function(){
 
-		var objeto = editimage.fabricaEditimageObjeto.criar();
+		var objeto = new EditimageObjeto(observer);
 
-		//expect(objeto.desenhar).toBeDefined();
+		expect(objeto.desenhar).toBeDefined();
+		expect('function').toEqual(typeof objeto.desenhar);
+
+	});
+
+	it('Deve retornar o Shape', function(){
+
+		var objeto = new EditimageObjeto(observer);
+
+		var shape = objeto.retornarShape();
+
+		expect(shape.graphics).toBeDefined();
+
+	});
+
+	it('Quando clicar em um elemento no canvas de ficar selecionado', function(){
+
+		var objeto = new EditimageObjeto(observer);
+
+		var shape = objeto.retornarShape();
+
+		shape.dispararEvento('click');
+
+		expect(true).toEqual(objeto.selecionado);
+
+	});
+
+	it('Quando o valor da propriedade selecionado for alterado deve disparar o observer', function(){
+
+		observer.notificado = false;
+
+		var objeto = new EditimageObjeto(observer);
+
+		objeto.selecionado = true;
+
+		expect(true).toEqual(observer.notificado);
+
+	});
+
+	it('Deve estourar um exceção ao criar um objeto sem observer', function(){
+
+
+		try{
+
+			new EditimageObjeto(observer);
+
+		}catch(ex){
+
+			expect(ex.message).toEqual('Informe o observer.');
+
+		}
+
+	});
+
+	it('Quando a propriedade selecionado for true, deve criar uma bordar azul', function(){
+
+		var objeto = new EditimageObjeto(observer);
+
+		objeto.selecionado = true;
+
+
 
 	});
 
